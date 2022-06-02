2328e54a5834efa74a0dade09e10bde16b3bd22a
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-06-02 19:47:33 +0900

    SRT実装の実験を開始。

--- workspace/poetry2-test.js
+++ workspace/poetry2-test.js
@@ -7,12 +7,15 @@ const canvas = new Canvas(cw, ch)
 canvas.fillStyle = "black"
 canvas.fillRect(0, 0, cw, ch)
 
-canvas.font = th + "px BIZUDPMincho-Regular"
+// canvas.font = th + "px BIZUDPMincho-Regular"
+canvas.font = th + "px VT323-Regular"
 canvas.textAlign = "center"
 canvas.textBaseline = "middle"
-canvas.fillStyle = "white"
-canvas.fillText("昭和横濱物語", cw / 2, ch / 2 - lh / 2)
-canvas.fillText("昭和横濱物語", cw / 2, ch / 2 + lh / 2)
+canvas.fillStyle = "#029D93"
+// canvas.fillText("昭和横濱物語", cw / 2, ch / 2 - lh / 2)
+// canvas.fillText("昭和横濱物語", cw / 2, ch / 2 + lh / 2)
+// canvas.fillText("SHOWA YOKOHAMA STORY if and only if...", cw / 2, ch / 2)
+canvas.fillText("EVANGELIUM SECUNDUM STEPHANUS re:verse", cw / 2, ch / 2)
 
 const output = new Output(canvas, 'output.png')
 output.addToQueue()
