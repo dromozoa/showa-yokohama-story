18341152251f5b97d5613408bac45f8efbec587d
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-06-04 15:12:06 +0900

    シェーダの実験を開始。

---
+++ love/conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+  t.gammacorrect = true
+  t.window.resizable = true
+  t.window.highdpi = true
+  t.window.title = "昭和横濱物語"
+  t.window.width = 1280
+  t.window.height = 720
+end
---
+++ love/main.lua
@@ -0,0 +1,54 @@
+local g = love.graphics
+
+local vt323
+local bizudpmincho
+local bg
+local shader
+
+--[[
+https://www.hitachihyoron.com/jp/pdf/1969/05/1969_05_08.pdf
+13型カラーブラウン管の寸法が載ってる。
+
+https://clemz.io/article-retro-shaders-webgl.html
+Retro Shaders with WebGL
+
+
+]]
+
+function love.load()
+  vt323 = assert(g.newFont(assert(love.font.newRasterizer "VT323-Regular.ttf")))
+  bizudpmincho = assert(g.newFont(assert(love.font.newRasterizer("BIZUDPMincho-Regular.ttf", 48))))
+  bg = assert(g.newImage(assert(love.image.newImageData "map2.png")))
+  canvas = assert(g.newCanvas(1280, 720))
+end
+
+function love.draw(dt)
+  local x, y, w, h = love.window.getSafeArea()
+  local font = bizudpmincho
+
+  g.setCanvas(canvas)
+  g.clear()
+  g.draw(bg, 0, 0)
+  g.printf("しずんでいく夕陽を、じっとながめていた。", font, x + 64, y + 128, w - 48)
+  g.printf("おわっていく世界を、じっと見つめていた。", font, x + 64, y + 224, w - 48)
+  g.printf("おわっていく昭和を、じっとにらんでいた。", font, x + 64, y + 320, w - 48)
+  g.setCanvas()
+
+  if shader then
+    g.setShader(shader)
+  end
+  g.draw(canvas, 0, 0)
+  if shader then
+    g.setShader()
+  end
+end
+
+function love.keyreleased(key)
+  if key == "s" then
+    g.captureScreenshot("test.png")
+  elseif key == "o" then
+    shader = assert(g.newShader "shader.txt")
+  elseif key == "c" then
+    shader = nil
+  end
+end
