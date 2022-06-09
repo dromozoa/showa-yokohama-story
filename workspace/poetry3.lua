5489c6b30470ce572f1fd13b11f309da30cb13d3
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-06-09 15:12:06 +0900

    スクリプトを修正。

--- workspace/poetry3.lua
+++ workspace/poetry3.lua
@@ -2,38 +2,27 @@
 
 package.path = "../love/?.lua;" .. package.path
 
-local dumper = require "dromozoa.commons.dumper"
-local ucd_kana = require "ucd_kana"
 local scenario = require "scenario"
 
 local scenario_filename = ...
 local basename = scenario_filename:gsub("%.[^%.]*", "")
 local srt_filename = basename .. "-vp.srt"
 
-local scenario_data = scenario.read(scenario_filename)
--- print(dumper.encode(scenario_data, { pretty = true, stable = true }))
+local data = scenario.read(scenario_filename)
 
 local out = assert(io.open(srt_filename, "w"))
 local counter = 0
-for i = 1, #scenario_data do
-  local data = scenario_data[i]
-
-  local seconds_per_measure = data.seconds_per_measure
-  local seconds_per_syllable = data.seconds_per_syllable
-  local measure = data.measure
-  local duration = data.duration
-
-  local t1 = measure * seconds_per_measure + duration * seconds_per_syllable
-  local t2 = (measure + 1) * seconds_per_measure
-
+for i = 1, #data do
+  local x = data[i]
   counter = counter + 1
   out:write(counter, "\n")
-  out:write(("%s --> %s\n"):format(scenario.format_time_srt(t1), scenario.format_time_srt(t2)))
+  out:write(("%s --> %s\n"):format(
+      scenario.format_time_srt(x.measure * x.seconds_per_measure + x.duration * x.seconds_per_syllable),
+      scenario.format_time_srt((x.measure + 1) * x.seconds_per_measure)))
 
-  for j = 1, #data do
-    local item = data[j]
-    local voice = item.voice
-    out:write(voice)
+  for j = 1, #x do
+    local y = x[j]
+    out:write(y.voice)
   end
   out:write "\n\n"
 end
