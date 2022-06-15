33f9a56f32169933d43601c2408ea487772eabe1
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-06-15 15:39:25 +0900

    話者ごとの分割を追加。

--- workspace/poetry3.lua
+++ workspace/poetry3.lua
@@ -5,10 +5,22 @@ package.path = "../love/?.lua;" .. package.path
 local dumper = require "dromozoa.commons.dumper"
 local scenario = require "scenario"
 
-local scenario_filename = ...
+local scenario_filename, speaker = ...
 local basename = scenario_filename:gsub("%.[^%.]*", "")
 local srt1_filename = basename .. "-vp.srt"
 local srt2_filename = basename .. ".srt"
+
+if speaker then
+  srt1_filename = basename .. "-vp-" .. speaker .. ".srt"
+end
+
+local function check_speaker(data_speaker)
+  if speaker then
+    return data_speaker == speaker
+  else
+    return true
+  end
+end
 
 local scenario_data, scenario_measures = scenario.read(scenario_filename)
 -- print(dumper.encode(scenario_data, { pretty = true, stable = true }))
@@ -19,7 +31,7 @@ local counter = 0
 for i = 1, #scenario_data do
   local data = scenario_data[i]
   local def = scenario.speakers[data.speaker]
-  if def.speaker then
+  if def.speaker and check_speaker(data.speaker) then
     counter = counter + 1
     out:write(counter, "\n")
     out:write(("%s --> %s\n"):format(
