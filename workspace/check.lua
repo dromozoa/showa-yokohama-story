31560e76c93afdaf6c1c95713d58fd85d083a87c
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-06-16 10:40:48 +0900

    スクリプトを修正。

--- workspace/check.lua
+++ workspace/check.lua
@@ -2,12 +2,32 @@
 
 local json = require "dromozoa.commons.json"
 
-local filename = ...
+local filename, mode = ...
 
 local handle = assert(io.open(filename, "rb"))
 local source = handle:read "*a" :gsub("\0$", "")
 handle:close()
 
 local root = json.decode(source)
-print(json.encode(root, { pretty = true }))
+
+if mode == "r" then
+  local blocks = root.project.blocks
+  for i = 1, #blocks do
+    local block = blocks[i]
+    local sentence_list = block["sentence-list"]
+    for j = 1, #sentence_list do
+      local sentence = sentence_list[j]
+      local tokens = sentence.tokens
+      print(sentence.text)
+      for k = 1, #tokens do
+        local token = tokens[k]
+        local r8 = token.r8
+        local r32 = token.r32
+        print(token.s, r8[1], r8[2], r32[1], r32[2])
+      end
+    end
+  end
+else
+  print(json.encode(root, { pretty = true }))
+end
 
