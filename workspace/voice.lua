c2a5bbf04cc0bc232ec5a4c24c68076f063ab7f8
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-06-17 07:36:27 +0900

    スクリプトを修正。

--- workspace/voice.lua
+++ workspace/voice.lua
@@ -1,61 +1,55 @@
 #! /usr/bin/env lua
 
-local wav_filename = ...
+local png = require "dromozoa.png"
+
+local wav_filename, png_filename = ...
 
 local handle = assert(io.open(wav_filename, "rb"))
 
-local function read_chunk_header(handle)
-  local tag = handle:read(4)
-  if not tag then
-    return
-  end
-  local size = string.unpack("<I4", handle:read(4))
-  return tag, size
-end
-
-local function read_fmt_chunk(handle, size)
-  local fmt = {}
-  fmt.format_tag,
-  fmt.channels,
-  fmt.samples_per_sec,
-  fmt.avg_bytes_per_sec,
-  fmt.block_align,
-  fmt.bits_per_sample = string.unpack("<I2I2I4I4I2I2", handle:read(size))
-  return fmt
-end
-
 assert(handle:read(4) == "RIFF")
 local size = string.unpack("<I4", handle:read(4))
-print(size)
+-- print(size)
 assert(handle:read(4) == "WAVE")
 
 local fmt
 local data = {}
+local size_before
+local size_main
+local size_after
+local size_padded
 
 while true do
-  local tag, size = read_chunk_header(handle)
+  local tag = handle:read(4)
   if not tag then
     break
   end
-  print(tag, size)
+  local size = string.unpack("<I4", handle:read(4))
+  -- print(tag, size)
+
   if tag == "fmt " then
-    fmt = read_fmt_chunk(handle, size)
+    fmt = {}
+    fmt.format_tag,
+    fmt.channels,
+    fmt.samples_per_sec,
+    fmt.avg_bytes_per_sec,
+    fmt.block_align,
+    fmt.bits_per_sample = string.unpack("<I2I2I4I4I2I2", handle:read(size))
     assert(fmt.format_tag == 1)
     assert(fmt.channels == 1)
     assert(fmt.block_align == 2)
   elseif tag == "data" then
-    local n = size / fmt.block_align
-    local m = 2 ^ math.ceil(math.log(n, 2))
-    local p = math.floor((m - n) / 2)
-    local q = m - n - p
+    size_main = size / fmt.block_align
+    size_padded = 2 ^ math.ceil(math.log(size_main, 2))
+    size_before = math.floor((size_padded - size_main) / 2)
+    size_after = size_padded - size_before - size_main
 
-    for i = 1, p do
+    for i = 1, size_before do
       data[i] = 0
     end
-    for i = p + 1, p + n do
+    for i = size_before + 1, size_before + size_main do
       data[i] = string.unpack("<i2", handle:read(2))
     end
-    for i = p + n + 1, m do
+    for i = size_before + size_main + 1, size_padded do
       data[i] = 0
     end
   else
@@ -63,15 +57,120 @@ while true do
   end
 end
 
-handle:close()
+-- Haar
+local X = data
+local n = #X
+local m = math.log(n, 2) - 1
+local result = {}
+local f = fmt.samples_per_sec
 
-local min = data[1]
-local max = data[1]
+for i = 1, m do
+  n = n / 2
+  local fmin = f / 2
 
-for i = 2, #data do
-  local v = data[i]
-  if min > v then min = v end
-  if max < v then max = v end
+  local C = {} -- 低周波成分
+  local D = { fmin = fmin, fmax = f } -- 高周波成分
+  f = fmin
+
+  local dmin = 0
+  local dmax = 0
+
+  for j = 1, n do
+    local k = j * 2
+    local a = X[k - 1]
+    local b = X[k]
+    local d = a - b
+    local c = a + d / 2
+    if dmin > d then dmin = d end
+    if dmax < d then dmax = d end
+    C[j] = c
+    D[j] = d
+  end
+  X = C
+  D.dmin = dmin
+  D.dmax = dmax
+  result[i] = D
 end
 
-print(#data, min, max)
+local C = X
+local D = result
+
+local rows
+local row_size = 100
+for i = 1, m do
+  local d = D[i]
+  if d.fmax < 200 then
+    rows = i
+    break
+  end
+end
+
+local t_size = 2048
+local t_scale = t_size / size_padded
+local t_size_before = math.floor(size_before * t_scale)
+local t_size_after = math.floor(size_after * t_scale)
+local t_size_main = t_size - t_size_before - t_size_after
+
+local out = assert(io.open(png_filename, "wb"))
+
+local writer = png.writer()
+writer:set_write_fn(function (data)
+  out:write(data)
+end, function ()
+  out:flush()
+end)
+writer:set_IHDR {
+  width = t_size_main;
+  height = rows * row_size;
+  bit_depth = 8;
+  color_type = png.PNG_COLOR_TYPE_RGB;
+}
+
+for i = 1, rows do
+  local d = D[i]
+  local v = {}
+  if #d == t_size then
+    v = d
+  elseif #d < t_size then
+    local s = t_size / #d
+    for j = 1, t_size do
+      v[j] = d[math.floor((j - 1) / s) + 1]
+    end
+  else
+    local s = #d / t_size
+    for j = 1, t_size do
+      local u = 0
+      for k = (j - 1) * s + 1, j * s do
+        u = u + d[k]
+      end
+      v[j] = u / s
+    end
+  end
+
+  local uscale = 255 / math.max(math.abs(d.dmin), math.abs(d.dmax))
+
+  local buffer = {}
+  for j = t_size_before + 1, t_size_before + t_size_main do
+    local u = v[j]
+    local x = math.floor(u * uscale)
+    if x == 0 then
+      buffer[#buffer + 1] = string.char(0x00, 0x00, 0x00)
+    elseif x > 0 then
+      buffer[#buffer + 1] = string.char(x, 0x00, 0x00)
+      -- buffer[#buffer + 1] = string.char(x, x, x)
+    else
+      buffer[#buffer + 1] = string.char(0x00, -x, 0x00)
+      -- buffer[#buffer + 1] = string.char(-x, -x, -x)
+    end
+  end
+
+  local buffer = table.concat(buffer)
+  for j = (i - 1) * row_size + 1, i * row_size do
+    writer:set_row(j, buffer)
+  end
+end
+
+writer:write_png()
+
+handle:close()
+out:close()
