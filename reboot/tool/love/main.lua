57a07a5425d4530b82fc550a95800765efc98719
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-11-08 15:54:19 +0900

    グリッチ風のアニメーションを調整。

--- reboot/tool/love/main.lua
+++ reboot/tool/love/main.lua
@@ -138,16 +138,19 @@ local function prepare_silhouette(pathname)
 
   local image = love.graphics.newImage(image_data)
   return {
+    pathaname = pathaname;
     line_data = line_data;
     image_data = image_data;
     image = image;
   }
 end
 
-function class.play(pathname1, pathname2)
+function class.play(...)
   game.frame = 0
-  game.silhouette1 = prepare_silhouette(pathname1)
-  game.silhouette2 = prepare_silhouette(pathname2)
+  game.silhouettes = {...}
+  for i, pathname in ipairs(game.silhouettes) do
+    game.silhouettes[i] = prepare_silhouette(pathname)
+  end
   game.font = love.graphics.newFont(love.font.newRasterizer("ShareTech-Regular.ttf", 20))
 end
 
@@ -159,23 +162,60 @@ function love.load(arg)
   end
 end
 
+local function compare_length(a, b)
+  local t = a.x2 - a.x1
+  local u = b.x2 - b.x1
+  if t == u then
+    return a.x1 < b.x1
+  else
+    return t < u
+  end
+end
+
+local function compare_x(a, b)
+  if a.x1 == b.x1 then
+    return a.x2 < b.x2
+  else
+    return a.x1 < b.x1
+  end
+end
+
 function love.draw()
   local g = love.graphics
+
+  local silhouettes = {
+    { 0, 1 }; { 1, 2 }; { 2, 1 };
+
+    { 0, 1 };
+    { 1, 1 };
+    { 1, 2 };
+    { 2, 2 };
+    { 2, 1 };
+    { 1, 0 };
+    { 0, 2 };
+    { 2, 0 };
+  }
+  local i = math.floor(game.frame / 120)
+  local j = i % #silhouettes + 1
+  local prev = silhouettes[j][1]
+  local this = silhouettes[j][2]
+  prev = game.silhouettes[prev]
+  this = game.silhouettes[this]
 
   -- 120フレームでアニメーションする。
   local t = (game.frame % 120) / 119
   local p = (1 - math.cos(math.pi * t)) * 0.5
-  local silhouette = game.silhouette1
-  local scale = 200
+  local q = 1 - p
+  local scale = 400
 
   local seed_x = 1000 * love.math.random()
   local seed_y = 1000 * love.math.random()
   local seed_z = 1000 * love.math.random()
 
   local colors = {
-    { 1, 0, 0 };
-    { 0, 1, 0 };
-    { 0, 0, 1 };
+    { 0.5, 0, 0 };
+    { 0, 0.5, 0 };
+    { 0, 0, 0.5 };
   }
 
   -- #029D93
@@ -185,23 +225,187 @@ function love.draw()
   --   { 0, 0, 0x93/0xFF };
   -- }
 
+  -- local colors = {
+  --   { 1, 1, 1 };
+  -- }
+
   g.clear()
   g.setLineWidth(1)
   g.setLineStyle "rough"
 
   local blend_mode = g.getBlendMode()
   g.setBlendMode "add"
-  for j = 1, #silhouette.line_data, 2 do
-    local lines = silhouette.line_data[j]
-    local y = lines.y
-    for i, line in ipairs(lines) do
-      for c, color in ipairs(colors) do
-        local x1 = line.x1
-        local x2 = line.x2
-        x1 = x1 + scale * (1 - p) * (love.math.noise(seed_x * x1, seed_y * y, seed_z * c) - 0.5) * 2
-        x2 = x2 + scale * (1 - p) * (love.math.noise(seed_x * x2, seed_y * y, seed_z * c) - 0.5) * 2
-        g.setColor(color[1] * p, color[2] * p, color[3] * p)
-        g.line(x1, y, x2, y)
+  if prev and this then
+    assert(#prev.line_data == #this.line_data)
+
+    if true then
+
+      for j = 1, #prev.line_data, 2 do
+        local prev_lines = prev.line_data[j]
+        local this_lines = this.line_data[j]
+        local y = this_lines.y
+
+        -- どちらかに線分がなければ、中点を基準とする。
+        if #this_lines == 0 then
+          for i, line in ipairs(prev_lines) do
+            for c, color in ipairs(colors) do
+              local x1 = line.x1
+              local x2 = line.x2
+              local x3 = (x1 + x2) * 0.5
+              x1 = x3 + (x1 - x3) * q
+              x2 = x3 + (x2 - x3) * q
+              x1 = x1 + scale * (1 - q) * (love.math.noise(seed_x * x1, seed_y * y, seed_z * c) - 0.5) * 2
+              x2 = x2 + scale * (1 - q) * (love.math.noise(seed_x * x2, seed_y * y, seed_z * c) - 0.5) * 2
+              g.setColor(color[1] * q, color[2] * q, color[3] * q)
+              -- g.setColor(color[1], color[2], color[3])
+              g.line(x1, y, x2, y)
+            end
+          end
+        elseif #prev_lines == 0 then
+          for i, line in ipairs(this_lines) do
+            for c, color in ipairs(colors) do
+              local x1 = line.x1
+              local x2 = line.x2
+              local x3 = (x1 + x2) * 0.5
+              x1 = x3 + (x1 - x3) * p
+              x2 = x3 + (x2 - x3) * p
+              x1 = x1 + scale * (1 - p) * (love.math.noise(seed_x * x1, seed_y * y, seed_z * c) - 0.5) * 2
+              x2 = x2 + scale * (1 - p) * (love.math.noise(seed_x * x2, seed_y * y, seed_z * c) - 0.5) * 2
+              g.setColor(color[1] * p, color[2] * p, color[3] * p)
+              -- g.setColor(color[1], color[2], color[3])
+              g.line(x1, y, x2, y)
+            end
+          end
+        else
+          local P = {}
+          local Q = {}
+          for i, line in ipairs(prev_lines) do
+            P[i] = { x1 = line.x1, x2 = line.x2 }
+          end
+          for i, line in ipairs(this_lines) do
+            Q[i] = { x1 = line.x1, x2 = line.x2 }
+          end
+
+          table.sort(P, compare_length)
+          table.sort(Q, compare_length)
+
+          -- 線分が少ないほうのいちばん長い線分を分割する
+          while #P < #Q do
+            local t = P[#P]
+            local x = (t.x1 + t.x2) * 0.5
+            local u = { x1 = x, x2 = t.x2 }
+            t.x2 = x
+            P[#P + 1] = u
+            table.sort(P, compare_length)
+          end
+
+          while #Q < #P do
+            local t = Q[#Q]
+            local x = (t.x1 + t.x2) * 0.5
+            local u = { x1 = x, x2 = t.x2 }
+            t.x2 = x
+            Q[#Q + 1] = u
+            table.sort(Q, compare_length)
+          end
+
+          table.sort(P, compare_x)
+          table.sort(Q, compare_x)
+
+          local r = (math.max(p, q) - 0.5) * 2
+          -- assert(r >= 0.5)
+
+          for i = 1, #P do
+            local prev_line = P[i]
+            local this_line = Q[i]
+
+            for c, color in ipairs(colors) do
+              local x1 = prev_line.x1 * q + this_line.x1 * p
+              local x2 = prev_line.x2 * q + this_line.x2 * p
+              x1 = x1 + scale * (1 - r) * (love.math.noise(seed_x * x1, seed_y * y, seed_z * c) - 0.5) * 2
+              x2 = x2 + scale * (1 - r) * (love.math.noise(seed_x * x2, seed_y * y, seed_z * c) - 0.5) * 2
+              g.setColor(color[1], color[2], color[3])
+              g.line(x1, y, x2, y)
+            end
+          end
+
+        end
+      end
+
+    else
+
+      for j = 1, #prev.line_data, 2 do
+        local lines = prev.line_data[j]
+        local y = lines.y
+        for i, line in ipairs(lines) do
+          for c, color in ipairs(colors) do
+            local x1 = line.x1
+            local x2 = line.x2
+            local x3 = (x1 + x2) * 0.5
+            x1 = x3 + (x1 - x3) * q
+            x2 = x3 + (x2 - x3) * q
+            x1 = x1 + scale * (1 - q) * (love.math.noise(seed_x * x1, seed_y * y, seed_z * c) - 0.5) * 2
+            x2 = x2 + scale * (1 - q) * (love.math.noise(seed_x * x2, seed_y * y, seed_z * c) - 0.5) * 2
+            g.setColor(color[1] * q, color[2] * q, color[3] * q)
+            -- g.setColor(color[1], color[2], color[3])
+            g.line(x1, y, x2, y)
+          end
+        end
+      end
+
+      for j = 1, #this.line_data, 2 do
+        local lines = this.line_data[j]
+        local y = lines.y
+        for i, line in ipairs(lines) do
+          for c, color in ipairs(colors) do
+            local x1 = line.x1
+            local x2 = line.x2
+            local x3 = (x1 + x2) * 0.5
+            x1 = x3 + (x1 - x3) * p
+            x2 = x3 + (x2 - x3) * p
+            x1 = x1 + scale * (1 - p) * (love.math.noise(seed_x * x1, seed_y * y, seed_z * c) - 0.5) * 2
+            x2 = x2 + scale * (1 - p) * (love.math.noise(seed_x * x2, seed_y * y, seed_z * c) - 0.5) * 2
+            g.setColor(color[1] * p, color[2] * p, color[3] * p)
+            g.line(x1, y, x2, y)
+          end
+        end
+      end
+
+    end
+
+  elseif prev then
+    for j = 1, #prev.line_data, 2 do
+      local lines = prev.line_data[j]
+      local y = lines.y
+      for i, line in ipairs(lines) do
+        for c, color in ipairs(colors) do
+          local x1 = line.x1
+          local x2 = line.x2
+          local x3 = (x1 + x2) * 0.5
+          x1 = x3 + (x1 - x3) * q
+          x2 = x3 + (x2 - x3) * q
+          x1 = x1 + scale * (1 - q) * (love.math.noise(seed_x * x1, seed_y * y, seed_z * c) - 0.5) * 2
+          x2 = x2 + scale * (1 - q) * (love.math.noise(seed_x * x2, seed_y * y, seed_z * c) - 0.5) * 2
+          g.setColor(color[1] * q, color[2] * q, color[3] * q)
+          g.line(x1, y, x2, y)
+        end
+      end
+    end
+  elseif this then
+    for j = 1, #this.line_data, 2 do
+      local lines = this.line_data[j]
+      local y = lines.y
+      for i, line in ipairs(lines) do
+        for c, color in ipairs(colors) do
+          local x1 = line.x1
+          local x2 = line.x2
+          local x3 = (x1 + x2) * 0.5
+          x1 = x3 + (x1 - x3) * p
+          x2 = x3 + (x2 - x3) * p
+          x1 = x1 + scale * (1 - p) * (love.math.noise(seed_x * x1, seed_y * y, seed_z * c) - 0.5) * 2
+          x2 = x2 + scale * (1 - p) * (love.math.noise(seed_x * x2, seed_y * y, seed_z * c) - 0.5) * 2
+          g.setColor(color[1] * p, color[2] * p, color[3] * p)
+          g.line(x1, y, x2, y)
+        end
       end
     end
   end
@@ -214,9 +418,9 @@ function love.draw()
 
   if game.running then
     game.frame = game.frame + 1
-    if game.frame == 120 then
+    if game.frame % 120 == 0 then
       game.running = false
-      game.frame = 119
+      game.frame = game.frame - 1
       local t = love.timer.getTime() - game.start
       game.start = nil
       print("elapsed: "..t)
@@ -235,8 +439,10 @@ function love.keypressed(key)
     game.fps = not game.fps
   elseif key == "s" then
     game.running = not game.running
-    game.frame = 0
-    game.start = love.timer.getTime()
+    if game.running then
+      game.frame = math.floor((game.frame + 1) / 120) * 120
+      game.start = love.timer.getTime()
+    end
   end
 end
 
