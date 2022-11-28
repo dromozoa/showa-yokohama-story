fb0d8f1aaaa0ba3629b41d139e704a2d9bdc8c92
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-11-28 22:41:16 +0900

    JavaScriptの開発プロジェクトを開始。

--- reboot/design2/common.js
+++ reboot/design2/common.js
@@ -121,7 +121,7 @@ const root = globalThis.dromozoa = new class {
   // Firefox 107でFontFace.load()の返り値およびFontFace.loadedが決定されない場
   // 合があった。どこかの時点でFontFaceが新しい別のオブジェクトにさしかえられ、
   // プロミスが迷子になるらしい。そこで、ポーリングしてdocument.facesを監視する
-  // することにした。
+  // ことにした。
   async load_font_face(index, timeout) {
     const font_face = [...document.fonts][index];
 
---
+++ reboot/design2/demeter.js
@@ -0,0 +1,204 @@
+// Copyright (C) 2022 Tomoyuki Fujimori <moyu@dromozoa.com>
+//
+// This file is part of 昭和横濱物語.
+//
+// 昭和横濱物語 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// 昭和横濱物語 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with 昭和横濱物語.  If not, see <http://www.gnu.org/licenses/>.
+
+(() => {
+"use strict";
+
+const D = globalThis.demeter ||= {};
+if (D.includeGuard) {
+  return;
+}
+D.includeGuard = true;
+
+//-------------------------------------------------------------------------
+
+D.requestAnimationFrame = () => new Promise(resolve => requestAnimationFrame(resolve));
+
+const escapeHTMLTable = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': "&quot;",
+  "'": "&apos;",
+};
+D.escapeHTML = source => source.replace(/[&<>"']/g, match => escapeHTMLTable[match]);
+
+D.createElement = html => {
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template.content.firstElementChild;
+};
+
+D.createElements = html => {
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template.content.children;
+};
+
+//-------------------------------------------------------------------------
+
+let bootException;
+let booted;
+let bootedFutures = [];
+
+D.boot = async () => {
+  return new Promise((resolve, reject) => {
+    if (booted) {
+      if (bootException === undefined) {
+        resolve();
+      } else {
+        reject(bootException);
+      }
+    } else {
+      bootedFutures.push({ resolve: resolve, reject: reject });
+    }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    initializeInternalRoot();
+    await Promise.all([ checkKerning() ]);
+  } catch (e) {
+    bootException = e;
+  }
+  booted = true;
+
+  if (bootException === undefined) {
+    bootedFutures.forEach(future => future.resolve());
+  } else {
+    bootedFutures.forEach(future => future.reject(bootException));
+  }
+  bootedFutures = undefined;
+}, { once: true });
+
+//-------------------------------------------------------------------------
+
+let internalRoot;
+let internalCanvas;
+let internalLayout;
+
+const initializeInternalRoot = () => {
+  internalRoot = document.body.appendChild(D.createElement(`
+    <div style="
+      position: absolute;
+      width: auto;
+      height: auto;
+      top: -1989px;
+      left: -6417px;
+    "></div>
+  `)).attachShadow({ mode: "closed" });
+
+  const view = internalRoot.appendChild(D.createElement(`
+    <div>
+      <div><canvas></canvas></div>
+      <div style="
+        position: relative;
+        width: auto;
+        height: auto;
+      "></div>
+    </div>
+  `));
+  internalCanvas = view.firstElementChild.firstElementChild;
+  internalLayout = view.children[1];
+};
+
+//-------------------------------------------------------------------------
+
+// Showa Yokohama Storyフォントは
+//   U+E001:   800/1000
+//   U+E002:   900/1000
+//   Kerning: -300/1000
+// という寸法を持つ。フォントサイズが100pxのとき、カーニングが効いていれば、文
+// 字列"\uE001\uE0002"の幅は140pxになる。
+const checkKerning = async () => {
+  const index = [...document.fonts].findIndex(fontFace => /Showa Yokohama Story/.test(fontFace.family));
+  if (index === -1) {
+    throw new Error("font-face 'Showa Yokohama Story' not found");
+  }
+  if ((await D.loadFontFace(index, 1000)).status !== "loaded") {
+    throw new Error("font-face 'Showa Yokohama Story' not loaded");
+  }
+
+  const context = internalCanvas.getContext("2d");
+  context.font = "100px 'Showa Yokohama Story'";
+  D.featureKerning = Math.abs(context.measureText("\uE001\uE002").width - 140) < 1;
+};
+
+//-------------------------------------------------------------------------
+
+// Firefox 107でFontFace.load()の返り値が（FontFace.loadedも）決定されない場合
+// があった。FontFaceが新しい別のオブジェクトにさしかえられ、プロミスが迷子にな
+// っているように見えた。そのため、ポーリングしてdocument.facesを監視することに
+// した。
+D.loadFontFace = async (index, timeout) => {
+  const fontFace = [...document.fonts][index];
+  if (fontFace.status === "loaded") {
+    return { status: "loaded", elapsed: 0 };
+  }
+  fontFace.load();
+
+  const start = await D.requestAnimationFrame();
+  if ([...document.fonts][index].status === "loaded") {
+    return { status: "loaded", elapsed: 0 };
+  }
+
+  if (!timeout || timeout < 0) {
+    timeout = Infinity;
+  }
+
+  while (true) {
+    const elapsed = await D.requestAnimationFrame() - start;
+    if ([...document.fonts][index].status === "loaded") {
+      return { status: "loaded", elapsed: elapsed };
+    }
+    if (timeout < elapsed) {
+      return { status: "timeout", elapsed: elapsed };
+    }
+  }
+};
+
+D.loadFontFaces = async (timeout) => {
+  const fontFaces = [...document.fonts].filter(fontFace => fontFace.status !== "loaded");
+  if (fontFaces.length === 0) {
+    return { status: "loaded", elapsed: 0 };
+  }
+  fontFaces.forEach(fontFace => fontFace.load());
+
+  const start = await D.requestAnimationFrame();
+  if ([...document.fonts].every(fontFace => fontFace.status === "loaded")) {
+    return { status: "loaded", elapsed: 0 };
+  }
+
+  if (!timeout || timeout < 0) {
+    timeout = Infinity;
+  }
+
+  while (true) {
+    const elapsed = await D.requestAnimationFrame() - start;
+    if ([...document.fonts].every(fontFace => fontFace.status === "loaded")) {
+      return { status: "loaded", elapsed: elapsed };
+    }
+    if (timeout < elapsed) {
+      return { status: "timeout", elapsed: elapsed };
+    }
+  }
+};
+
+//-------------------------------------------------------------------------
+
+})();
