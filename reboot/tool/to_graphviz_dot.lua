fad671690eb692ee4fa689ab923d68ef81fc2bdd
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-12-13 13:50:42 +0900

    シナリオのdot出力を修正。

--- reboot/tool/to_graphviz_dot.lua
+++ reboot/tool/to_graphviz_dot.lua
@@ -20,35 +20,35 @@ local parse = require "parse"
 --[[
 
 シナリオの構造をGraphvizのdot形式で出力する。
-変換結果のSVGを整理する。
 
 SVGの単位系は
   1in = 72pt = 90px
-
 GraphvizのSVG出力はpt単位で行われる。
 72dpi以外を設定すると、<svg>直下の<g>でscaleがかかる。
 
 基本高さ
   8pt = 0.11111111in
 
-基本幅 (box)
-  24pt = 0.33333333in
+種別 [c]
+  分岐の幅 (diamond)
+    36pt = 0.5in
 
-分岐の幅 (diamond)
-  36pt = 0.5in
+種別 [p]
+  段落の幅 (box)
+    24pt = 0.33333333in
+  選択肢付き段落の幅 (trapezium)
+    30pt = 0.41666667in
 
-選択肢の幅 (trapezium)
-  30pt = 0.41666667in
+種別 [f]
+  終端の直径 (circle)
+    12pt = 0.16666667in
 
-終端の直径 (circle)
-  12pt = 0.16666667in
+矢印はSVGのmark-endで実現し、Graphvizでは描画しない。
 
 ]]
 
 local scenario_pathname, result_pathname = ...
 local scenario = parse(scenario_pathname)
-
-local handle = assert(io.open(result_pathname, "w"))
 
 local function get_entry(paragraph)
   if paragraph.when_jumps then
@@ -58,8 +58,13 @@ local function get_entry(paragraph)
   end
 end
 
-local E = "--"
+local function jump_entry(jump)
+  return get_entry(scenario[scenario.labels[jump.label].index])
+end
 
+local to = "--"
+
+local handle = assert(io.open(result_pathname, "w"))
 handle:write [[
 graph {
 dpi=72;
@@ -71,27 +76,27 @@ for i, paragraph in ipairs(scenario) do
   if paragraph.when_jumps then
     handle:write("c", i, "[shape=diamond,width=0.5];\n")
     for _, jump in ipairs(paragraph.when_jumps) do
-      handle:write("c", i, E, get_entry(scenario[scenario.labels[jump.label].index]), ";\n")
+      handle:write("c", i, to, jump_entry(jump), ";\n")
     end
-    handle:write("c", i, E, "p", i, ";\n")
+    handle:write("c", i, to, "p", i, ";\n")
   end
 
   if paragraph.choice_jumps then
     handle:write("p", i, "[shape=trapezium,width=0.41666667];\n");
     for _, jump in ipairs(paragraph.choice_jumps) do
-      handle:write("p", i, E, get_entry(scenario[scenario.labels[jump.label].index]), ";\n")
+      handle:write("p", i, to, jump_entry(jump), ";\n")
     end
   else
     handle:write("p", i, ";\n");
     if paragraph.jump then
-      local jump = paragraph.jump
-      handle:write("p", i, E, get_entry(scenario[scenario.labels[jump.label].index]), ";\n")
+      handle:write("p", i, to, jump_entry(paragraph.jump), ";\n")
     elseif paragraph.finish then
       handle:write("f", i, "[shape=circle,width=0.16666667,height=0.16666667];\n");
-      handle:write("p", i, E, "f", i, ";\n");
+      handle:write("p", i, to, "f", i, ";\n");
     else
-      handle:write("p", i, E, get_entry(scenario[i + 1]), ";\n")
+      handle:write("p", i, to, get_entry(scenario[i + 1]), ";\n")
     end
   end
 end
 handle:write "}\n"
+handle:close()
