d412c22b3d247d154022f2a4916dea86df0eb204
Tomoyuki Fujimori <moyu@dromozoa.com>
2023-03-21 13:16:32 +0900

    コピーライトのメールアドレス修正。

--- reboot/design/tomography.lua
+++ reboot/design/tomography.lua
@@ -1,6 +1,6 @@
 #! /usr/bin/env lua
 
--- Copyright (C) 2022 Tomoyuki Fujimori <moyu@dromozoa.com>
+-- Copyright (C) 2022 Tomoyuki Fujimori <moyu@vaporoid.com>
 --
 -- This file is part of 昭和横濱物語.
 --
--- reboot/tool/convert_voice.sh
+++ reboot/tool/convert_voice.sh
@@ -1,6 +1,6 @@
 #! /bin/sh -e
 
-# Copyright (C) 2022 Tomoyuki Fujimori <moyu@dromozoa.com>
+# Copyright (C) 2022 Tomoyuki Fujimori <moyu@vaporoid.com>
 #
 # This file is part of 昭和横濱物語.
 #
