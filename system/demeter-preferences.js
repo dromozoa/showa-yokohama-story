55f31b3b8994bf827eb969dea3e960059a61bd3a
Tomoyuki Fujimori <moyu@dromozoa.com>
2023-04-02 06:57:33 +0900

    バージョンを更新。

--- system/demeter-preferences.js
+++ system/demeter-preferences.js
@@ -24,7 +24,7 @@ if (D.preferenes) {
 }
 
 const mode = "develop";
-const version = { web: "b9", system: 7, music: 1, voice: 3 };
+const version = { web: "b10", system: 8, music: 1, voice: 3 };
 
 if (mode === "develop") {
   D.preferences = {
--- version.mk
+++ version.mk
@@ -1,4 +1,4 @@
-version_web = b9
-version_system = 7
+version_web = b10
+version_system = 8
 version_music = 1
 version_voice = 3
