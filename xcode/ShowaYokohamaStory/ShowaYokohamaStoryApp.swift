0e8602967921f9d363d321d95114ad430b040772
Tomoyuki Fujimori <moyu@dromozoa.com>
2023-04-11 11:10:40 +0900

    リファクタリング。

--- xcode/ShowaYokohamaStory/ShowaYokohamaStoryApp.swift
+++ xcode/ShowaYokohamaStory/ShowaYokohamaStoryApp.swift
@@ -19,9 +19,9 @@ import SwiftUI
 
 @main
 struct ShowaYokohamaStoryApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
