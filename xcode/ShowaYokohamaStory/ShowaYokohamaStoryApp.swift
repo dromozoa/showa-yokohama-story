a2cd7c566b3adf93c403564d3d3809890da08a57
Tomoyuki Fujimori <moyu@dromozoa.com>
2023-04-11 10:51:48 +0900

    空のxcodeプロジェクトを追加。

---
+++ xcode/ShowaYokohamaStory/ShowaYokohamaStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowaYokohamaStoryApp.swift
+//  ShowaYokohamaStory
+//
+//  Created by moyu on 2023/04/11.
+//
+
+import SwiftUI
+
+@main
+struct ShowaYokohamaStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
