05554209e78a73f115892708eae52e965bf04867
Tomoyuki Fujimori <moyu@dromozoa.com>
2023-04-11 11:44:38 +0900

    SwiftUIでWebViewを使う準備。

---
+++ docs/APP.md
@@ -0,0 +1,10 @@
+# アプリ開発
+
+iOSから作る。
+
+## WKWebView
+
+## Google AdMob
+
+- 参考: https://developers.google.com/admob/ios/quick-start
+
--- xcode/ShowaYokohamaStory/ContentView.swift
+++ xcode/ShowaYokohamaStory/ContentView.swift
@@ -16,6 +16,42 @@
 // along with 昭和横濱物語.  If not, see <http://www.gnu.org/licenses/>.
 
 import SwiftUI
+import WebKit
+
+#if os(iOS)
+  typealias WebViewRepresentable = UIViewRepresentable
+#elseif os(macOS)
+  typealias WebViewRepresentable = NSViewRepresentable
+#endif
+
+struct WebView: WebViewRepresentable {
+  #if os(iOS)
+    func makeUIView(context: Context) -> WKWebView {
+      return makeView(context: context)
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+      return updateView(uiView, context: context)
+    }
+  #elseif os(macOS)
+    func makeNSView(context: Context) -> WKWebView {
+      return makeView(context: context)
+    }
+
+    func updateNSView(_ uiView: WKWebView, context: Context) {
+      return updateView(uiView, context: context)
+    }
+  #endif
+}
+
+extension WebView {
+  func makeView(context: Context) -> WKWebView {
+    return WKWebView()
+  }
+
+  func updateView(_ uiView: WKWebView, context: Context) {
+  }
+}
 
 struct ContentView: View {
   var body: some View {
