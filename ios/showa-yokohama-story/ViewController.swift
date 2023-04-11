6379c261cf0c62b69da96903a85c6935f7025c1e
Tomoyuki Fujimori <moyu@dromozoa.com>
2023-04-11 17:34:55 +0900

    iOSアプリのプロジェクトを追加。

---
+++ ios/showa-yokohama-story/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  showa-yokohama-story
+//
+//  Created by moyu on 2023/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
