1b8c655f8b1b2c3dd3743d037d59f8c30ed09319
Tomoyuki Fujimori <moyu@dromozoa.com>
2023-04-14 15:10:38 +0900

    リファクタリング。

--- android/app/src/main/java/com/vaporoid/sys/MainActivity.java
+++ android/app/src/main/java/com/vaporoid/sys/MainActivity.java
@@ -22,7 +22,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- android/app/src/test/java/com/vaporoid/sys/ExampleUnitTest.java
+++ android/app/src/test/java/com/vaporoid/sys/ExampleUnitTest.java
@@ -21,11 +21,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
